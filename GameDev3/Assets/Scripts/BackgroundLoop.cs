1fc07e59608ba59f2cf9d53531c5181fd1b2466c
marcosss3 <kcts1g13@soton.ac.uk>
2016-04-30 20:09:39 +0100

    kkk

---
+++ GameDev3/Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundLoop : MonoBehaviour {
+
+
+	SpriteRenderer sprite;
+
+	void Awake () {
+
+		// Get the current sprite with an unscaled size
+		sprite = GetComponent<SpriteRenderer>();
+		Vector2 spriteSize = new Vector2(sprite.bounds.size.x / transform.localScale.x, sprite.bounds.size.y / transform.localScale.y);
+
+		// Generate a child prefab of the sprite renderer
+		GameObject childPrefab = new GameObject();
+		SpriteRenderer childSprite = childPrefab.AddComponent<SpriteRenderer>();
+		childPrefab.transform.position = transform.position;
+		childSprite.sprite = sprite.sprite;
+
+		GameObject child;
+
+		// Loop through and spit out repeated tiles
+		for (int i = 1, l = (int)Mathf.Round(sprite.bounds.size.x + 1); i < l; i++) {
+			child = Instantiate(childPrefab) as GameObject;
+			child.transform.position = transform.position + (new Vector3(spriteSize.x - 1, 0, 0) * i);
+			child.transform.parent = transform;
+		}
+
+		// Set the parent last on the prefab to prevent transform displacement
+		childPrefab.transform.parent = transform;
+
+		// Disable the currently existing sprite component since its now a repeated image
+		sprite.enabled = false;
+
+	}
+
+}
---
+++ GameDev3/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private bool grounded, interact, keysEnabled, invincible;
+
+	public Animator anim;
+	public float speed = 4.0f;
+	public float jumpPower = 350f;
+
+	float jumpTime, jumpDelay = 0.3f;
+	bool jumped;
+
+	void Start() {
+		
+		rb = GetComponent<Rigidbody2D> ();
+
+		keysEnabled = true;
+		grounded = true;
+
+	}
+
+	void Update() {
+		
+		Movement(); //call the function every frame
+
+	}
+
+	void Movement() {
+
+		if (keysEnabled) {
+
+			if (rb.velocity.y < 0 && !grounded) {
+				anim.SetBool ("playerAirborne", false);
+				Debug.Log ("now");
+				anim.SetBool ("playerFalling", true);
+			}
+
+			if (Input.GetKey (KeyCode.D)) {
+				transform.Translate (Vector3.right * speed * Time.deltaTime); 
+				transform.eulerAngles = new Vector2 (0, 0); // Sets the rotation of the gameobject
+				if(grounded)
+					anim.SetBool("playerRunning", true);
+			}
+			if (Input.GetKey (KeyCode.A)) {
+				transform.Translate (Vector3.right * speed * Time.deltaTime);
+				transform.eulerAngles = new Vector2 (0, 180); // Sets the rotation of the gameobject
+				if(grounded)
+					anim.SetBool("playerRunning", true);
+			}
+
+			if (Input.GetKeyDown (KeyCode.W) && grounded) {
+				rb.AddForce (transform.up * jumpPower);
+				grounded = false;
+				anim.SetBool ("playerAirborne", true);
+			}
+
+			if (Input.GetMouseButtonDown (0) && grounded && !anim.GetBool("playerRunning")) {
+				anim.SetBool ("playerShooting", true);
+			}
+
+			if (Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.A)) {
+				anim.SetBool ("playerRunning", false);
+			}
+
+		}
+
+	}
+
+	public void StopFiring(){
+
+		anim.SetBool ("playerShooting", false);
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+
+		if (collision.gameObject.tag == "Ground") {
+
+			grounded = true;
+			anim.SetBool ("playerFalling", false);
+			anim.SetBool ("playerAirborne", false);
+
+		}
+
+	}
+
+}
