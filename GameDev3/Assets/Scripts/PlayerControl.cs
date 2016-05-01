8c7821aeb3efc62347202c20efaeafd883b9c734
marcosss3 <kcts1g13@soton.ac.uk>
2016-05-01 21:24:02 +0100

    Player orientation follows mouse

--- GameDev3/Assets/Scripts/PlayerControl.cs
+++ GameDev3/Assets/Scripts/PlayerControl.cs
@@ -10,13 +10,18 @@ public class PlayerControl : MonoBehaviour {
 
 	private float initialX = -16.0f;
 	private float invincibleTimeAfterHurt = 2;
+	private int ammo = 20;
 
 	private Stopwatch stopWatch;
 
 
 	public Animator anim;
+	public GameObject bullet;
+
 	public float speed = 4.0f;
 	public float jumpPower = 350f;
+
+	public Camera camera;
 
 	bool jumped;
 
@@ -68,11 +73,18 @@ public class PlayerControl : MonoBehaviour {
 
 			if (Input.GetMouseButtonDown (0) && grounded && !anim.GetBool("playerRunning")) {
 				anim.SetBool ("playerShooting", true);
+				Shoot ();
 			}
 
 			if (Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.A)) {
 				anim.SetBool ("playerRunning", false);
 			}
+
+			if (camera.ScreenToWorldPoint (Input.mousePosition).x > rb.position.x)
+				transform.eulerAngles = new Vector2 (0, 0);
+
+			if (camera.ScreenToWorldPoint (Input.mousePosition).x < rb.position.x)
+				transform.eulerAngles = new Vector2 (0, 180);
 
 		}
 
@@ -142,17 +154,37 @@ public class PlayerControl : MonoBehaviour {
 
 		invincible = true;
 
-		// Dizzy to just blink
-		yield return new WaitForSeconds(0.1F);
-		anim.SetBool("Blink", true);
+		anim.SetBool("playerBlinking", true);
 
 		// Waiting for invincibility to end
 		yield return new WaitForSeconds(hurtTime);
 
 		// Stop blinking animation and re-enable collision
-		anim.SetBool ("Blink", false);
+		anim.SetBool ("playerBlinking", false);
 
 		invincible = false;
+
+	}
+
+	void Shoot (){
+		
+		if(ammo > 0){
+
+			var pos = Input.mousePosition;
+			pos.z = transform.position.z - Camera.main.transform.position.z;
+			pos = Camera.main.ScreenToWorldPoint(pos);
+
+			var q = Quaternion.FromToRotation(Vector3.up, pos - transform.position);
+			GameObject go = Instantiate(bullet, transform.position, q) as GameObject;
+			Rigidbody2D gorb = go.GetComponent<Rigidbody2D> ();
+
+			gorb.AddForce (gorb.transform.up * 500);
+
+			//bulletInstance.GetComponent<Rigidbody2D> ().velocity = new Vector2 (10.0f, bulletInstance.GetComponent<Rigidbody2D> ().velocity.y + 0.2f);
+
+			ammo--;
+
+		}
 
 	}
 
